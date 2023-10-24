1453b4ad5a7ea520f9b14a3817eefc4476ac08fe
Were Samson Bruno <werefast2000@gmail.com>
2023-10-24 11:06:44 +0000

    created User file

---
+++ User.java
@@ -0,0 +1,22 @@
+public class User {
+    // first name of the user
+     String firstName = "Bright"; 
+     String lastName = "Ahedor";
+
+    int age = 23;
+    boolean hasMoney = true;
+
+    private static void presentName(){
+
+                double amountPaid = 23.05;
+        double amountCredited = 12;
+        double result = amountPaid/amountCredited;
+        
+        int amountConverted = (int) amountPaid;
+
+        System.out.print("New Value:" + amountConverted );
+        System.out.println("Result:" + result );
+
+    }
+
+    }
