90ad616c53e96d4bd7439f166fdae79db0cd7652
Were Samson Bruno <werefast2000@gmail.com>
2023-11-03 19:55:04 +0000

    created Main class with instances of Animal class

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        Animal cat = new Animal();
+        Animal cow = new Animal();
+    }
+    
+}
